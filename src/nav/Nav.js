9b33294d17d0873d5c72f986079b655e810468db
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2024-03-14 23:42:31 +0600

    updade nav.js

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -6,50 +6,44 @@ import stack from "../assets/stack.png";
 import envelope from "../assets/envelope.png";
 import "../styles/nav.css";
 
-export default Nav() {
-    const useLocation = useLocation();
-    const getNavPositionclass = () => { 
-        switch (useLocation.pathname) {
-            case "/":
-                return "nav-about";
-            case "/skills":
-                return "nav-skills";
-            case "/projects":
-                return "nav-projects";
-            case "/contact":
-                return "nav-contact";
-            default:
-                return "";
-
-        }
+export default function Nav() {
+  const useLocation = useLocation();
+  const getNavPositionclass = () => {
+    switch (useLocation.pathname) {
+      case "/":
+        return "nav-about";
+      case "/skills":
+        return "nav-skills";
+      case "/projects":
+        return "nav-projects";
+      case "/contact":
+        return "nav-contact";
+      default:
+        return "";
     }
+  };
 
-    const getPageTitle = () => {
-        switch (useLocation.pathname) {
-            case "/":
-                return "ABOUT";
-            case "/skills":
-                return "SKILLS";
-            case "/projects":
-                return "PROJECTS";
-            case "/contact":
-                return "CONTACT";
-            default:
-                return "";
-
-        }
+  const getPageTitle = () => {
+    switch (useLocation.pathname) {
+      case "/":
+        return "ABOUT";
+      case "/skills":
+        return "SKILLS";
+      case "/projects":
+        return "PROJECTS";
+      case "/contact":
+        return "CONTACT";
+      default:
+        return "";
     }
+  };
 
-    const NavPositionclass = getNavPositionclass();
-    const pageTitle = getPageTitle();
-  render() {
-    return (
-      <>
-        <img src={astronautHelmet} />
-        <img src={deadEye} />
-        <img src={stack} />
-        <img src={envelope} />
-      </>
-    );
-  }
+  const NavPositionclass = getNavPositionclass();
+  const pageTitle = getPageTitle();
+
+  const isCurrentPage = (navClass) => {
+    return navClass === NavPositionclass;
+  };
+
+  const renderNavLink = (to, imgSrc, altText, navClass) => {};
 }
