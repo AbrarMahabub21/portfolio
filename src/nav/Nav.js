6da3cc04f97de40aa21534536266e2034dc11e7c
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2024-03-14 19:19:30 +0600

    .

---
+++ src/nav/Nav.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Link, useLocation } from "react-router-dom";
+import astronautHelmet from "../assets/astronaut-helmet.png";
+import deadEye from "../assets/dead-eye.png";
+import stack from "../assets/stack.png";
+import envelope from "../assets/envelope.png";
+import "../styles/nav.css";
+
+export default Nav() {
+    const useLocation = useLocation();
+    const getNavPositionclass = () => { 
+        switch (useLocation.pathname) {
+            case "/":
+                return "nav-about";
+            case "/skills":
+                return "nav-skills";
+            case "/projects":
+                return "nav-projects";
+            case "/contact":
+                return "nav-contact";
+            default:
+                return "";
+
+        }
+    }
+
+    const getPageTitle = () => {
+        switch (useLocation.pathname) {
+            case "/":
+                return "ABOUT";
+            case "/skills":
+                return "SKILLS";
+            case "/projects":
+                return "PROJECTS";
+            case "/contact":
+                return "CONTACT";
+            default:
+                return "";
+
+        }
+    }
+
+    const NavPositionclass = getNavPositionclass();
+    const pageTitle = getPageTitle();
+  render() {
+    return (
+      <>
+        <img src={astronautHelmet} />
+        <img src={deadEye} />
+        <img src={stack} />
+        <img src={envelope} />
+      </>
+    );
+  }
+}
